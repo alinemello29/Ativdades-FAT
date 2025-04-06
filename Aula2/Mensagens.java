578ff7b701d100fcc8ac6734c42ab63a4f4265d3
alinemello29 <alinepe2717@gmail.com>
2025-04-06 15:33:54 -0300

    ☕

---
+++ Aula2/Mensagens.java
@@ -0,0 +1,13 @@
+public class Mensagens {
+    public static void main(String[] args) {
+        // Exibindo a primeira mensagem
+        System.out.println("Boa");
+        System.out.println("Noite");
+        System.out.println("!");
+
+        // Exibindo o padrão de asteriscos
+        System.out.println("*****");
+        System.out.println("* A *");
+        System.out.println("*****");
+    }
+}
