dca20ee6eb400f4e2fd45d77b2702043ec7e655f
alinemello29 <alinepe2717@gmail.com>
2025-04-07 20:39:08 -0300

    ☕

---
+++ Aula3/ExibicaoVariaveis.java
@@ -0,0 +1,11 @@
+public class ExibicaoVariaveis {
+    public static void main(String[] args) {
+        // Declaração das variáveis
+        String nome = "JOSE"; // Substitua "JOSE" pelo seu nome
+        int idade = 44;       // Substitua 44 pela sua idade
+
+        // Exibindo as mensagens
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Eu tenho " + idade + " anos.");
+    }
+}
